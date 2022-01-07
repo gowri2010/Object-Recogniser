654c3ba03e836c386923ff213d10b6f95a295f2f
gowri2010 <86656713+gowri2010@users.noreply.github.com>
2022-01-07 19:25:51 +1000

    Update main.js

--- main.js
+++ main.js
@@ -13,21 +13,21 @@ Webcam.attach('#camera' );
 
 function take_snapshot()
 {
-    Webacam.snap(function(data_url) {
+    Webacam.snap(function(data_uri) {
      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     })
 }
 
 
 console.log('ml5 version', ml5.version);
-classfier = ml5.imageClassfier("https://teachablemachine.withgoogle.com/models/J1X3V-4f1/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/J1X3V-4f1/model.json", modelLoaded);
 function modelLoaded() {
 console.log('Model Loaded!');
 }
 
 function check(){
 img = document.getElementById('captured_image');
-classfier.classify(img, gotResult);
+classifier.classify(img, gotResult);
 }
 
 function gotResult(error, results) {
